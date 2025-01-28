c22b6bdfab1acbadec54f876e637aedda76c0a45
sirTomasson <gretch552@gmail.com>
2025-01-28 12:31:10 +0100

    Updates to compile benchmarks

--- compile_benchmark.py
+++ compile_benchmark.py
@@ -56,14 +56,15 @@ def main():
     cfg = Config()
     cfg.SLOWFAST.ALPHA = 4
     cfg.DATA.DATASET_PATH = '/home/s2871513/Datasets/Diving48'
+    cfg.DATA.NUM_FRAMES = 16
     N_ITERS = 10
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = init_model(cfg, device)
     criterion, optimiser, *_ = train_helper.get_train_objects(cfg, model)
 
-    def train(mod, xb, yb):
+    def train(mod, xb, yb, scaler=None):
         optimiser.zero_grad(True)
-        pred = train_helper.forward(mod, xb, device, cfg)
+        pred = train_helper.forward(mod, xb, device, cfg, scaler)
         loss = criterion(pred, yb)
         loss.backward()
         optimiser.step()
@@ -71,29 +72,65 @@ def main():
     eager_times = []
     for i in range(N_ITERS):
         xb, yb = generate_data(16)
-        train_helper.forward(model, xb, device, cfg)
-        print(xb.shape, yb.shape)
+        yb = yb.to(device)
         _, eager_time = timed(lambda: train(model, xb, yb))
         eager_times.append(eager_time)
         print(f"eager train time {i}: {eager_time}")
     print("~" * 10)
 
     model = init_model(cfg, device)
-    train_opt = torch.compile(train, mode="reduce-overhead")
+    criterion, optimiser, *_ = train_helper.get_train_objects(cfg, model)
+    train_opt = torch.compile(model, mode="reduce-overhead")
 
     compile_times = []
     for i in range(N_ITERS):
         xb, yb = generate_data(16)
+        yb = yb.to(device)
         _, compile_time = timed(lambda: train(train_opt, xb, yb))
         compile_times.append(compile_time)
         print(f"compile train time {i}: {compile_time}")
     print("~" * 10)
 
+
+    cfg.TRAIN.AMP = True
+    model = init_model(cfg, device)
+    criterion, optimiser, _, _, scaler = train_helper.get_train_objects(cfg, model)
+
+    amp_times = []
+    for i in range(N_ITERS):
+        xb, yb = generate_data(16)
+        yb = yb.to(device)
+        _, amp_time = timed(lambda: train(model, xb, yb, scaler))
+        amp_times.append(amp_time)
+        print(f"amp train time {i}: {amp_time}")
+    print("~" * 10)
+
+
+    model = init_model(cfg, device)
+    criterion, optimiser, _, _, scaler = train_helper.get_train_objects(cfg, model)
+    train_opt = torch.compile(model, mode="reduce-overhead")
+
+    amp_compile_times = []
+    for i in range(N_ITERS):
+        xb, yb = generate_data(16)
+        yb = yb.to(device)
+        _, amp_compile_time = timed(lambda: train(model, xb, yb, scaler))
+        amp_compile_times.append(amp_compile_time)
+        print(f"amp train time {i}: {amp_compile_time}")
+    print("~" * 10)
+
     eager_med = np.median(eager_times)
     compile_med = np.median(compile_times)
+    amp_med = np.median(amp_times)
+    amp_compile_med = np.median(amp_compile_times)
     speedup = eager_med / compile_med
-    assert (speedup > 1)
+    speedup_amp = eager_med / amp_med
+    speedup_amp_compile = eager_med / amp_compile_med
+    # assert (speedup > 1)
+
     print(f"(train) eager median: {eager_med}, compile median: {compile_med}, speedup: {speedup}x")
+    print(f"(train) eager median: {eager_med}, amp median: {amp_med}, speedup: {speedup_amp}x")
+    print(f"(train) eager median: {eager_med}, amp+compile median: {amp_compile_med}, speedup: {speedup_amp_compile}x")
     print("~" * 10)
 
 if __name__ == '__main__':
