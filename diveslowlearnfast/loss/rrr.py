a2a8da4876be0031fb997d74a3340796371f5c1b
Youri Tomassen <gretch552@gmail.com>
2025-02-11 14:42:18 +0100

    Create an optimised RRRLoss that removes the L2 regularisation in favor of using weight decay. More importantly, reduce the overhead of calculating gradients for the input with respect to the summed logprobs by filtering the masks that have sums greater than 1.

--- diveslowlearnfast/loss/rrr.py
+++ diveslowlearnfast/loss/rrr.py
@@ -1,10 +1,11 @@
+import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class RRRLoss(nn.Module):
-    def __init__(self, lambda1=1000.0, lambda2=0.0001):
+    def __init__(self, lambda1=1000.0):
         """
         Right for the Right Reasons loss function.
 
@@ -18,14 +19,12 @@ class RRRLoss(nn.Module):
 
         Args:
             lambda1: Weight for the explanation (input gradient) loss term
-            lambda2: Weight for the L2 regularization term
         """
         super().__init__()
         self.lambda1 = lambda1
-        self.lambda2 = lambda2
         self.cross_entropy = nn.CrossEntropyLoss()
 
-    def forward(self, logits, targets, model, inputs, masks):
+    def forward(self, logits, targets, inputs, masks):
         """
         Compute the RRR loss.
 
@@ -40,43 +39,29 @@ class RRRLoss(nn.Module):
             total_loss: Combined loss value
             losses: Dictionary containing individual loss components
         """
-        batch_size = logits.size(0)
-
-        # Right answers loss (cross entropy)
         ce_loss = self.cross_entropy(logits, targets)
+        # boolean mask to select indices for which there is a mask available
+        # this little optimisation ensures that we do not use autograd on inputs that will
+        # be ignored anyway
+        mask = (masks.sum(dim=(1, 2, 3, 4)) > 0)
+        masked_elements = inputs[mask]
 
-        # Right reasons loss (input gradient penalty)
-        log_probs = F.log_softmax(logits, dim=1)
+        if masked_elements.shape[0] > 0:
+            log_probs = F.softmax(masked_elements, dim=1)
+            summed_log_probs = log_probs.sum()
+            gradients = torch.autograd.grad(summed_log_probs, inputs, create_graph=True, retain_graph=True)[0]
+            gradient_loss = (masks * gradients).pow(2).mean()
+            gradient_loss_item = gradient_loss.item()
+        else:
+            gradient_loss = 0
+            gradient_loss_item = gradient_loss
 
-        # Sum across classes for each sample
-        summed_log_probs = log_probs.sum(dim=1)
+        total_loss = ce_loss + self.lambda1 * gradient_loss
 
-        # Compute input gradients
-        gradients = torch.autograd.grad(
-            summed_log_probs.sum(),
-            inputs,
-            create_graph=True,
-            retain_graph=True
-        )[0]
-
-        # Apply mask and compute mean squared gradients
-        masked_gradients = gradients * masks
-        gradient_loss = (masked_gradients ** 2).mean()
-
-        # L2 regularization on model parameters
-        l2_loss = 0.0
-        for param in model.parameters():
-            l2_loss += torch.sum(param ** 2)
-
-        # Combine losses
-        total_loss = ce_loss + self.lambda1 * gradient_loss + self.lambda2 * l2_loss
-
-        # Store individual loss components for logging
         losses = {
             'total_loss': total_loss.item(),
             'ce_loss': ce_loss.item(),
-            'gradient_loss': gradient_loss.item(),
-            'l2_loss': l2_loss.item()
+            'gradient_loss': gradient_loss_item
         }
 
         return total_loss, losses
